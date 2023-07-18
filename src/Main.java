812be808b85204d45c15f89a1300b6010508478f
Katerina Voroshilova <kotovaekaterina1@gmail.com>
2023-07-18 14:04:35 +0300

    Task1 add

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        int ticketPrice = 13676;
+        int bonus = 20;
+        int bonusMile = ticketPrice/bonus;
+        System.out.println("Количество бонусных миль равно "+bonusMile);
+    }
+}
